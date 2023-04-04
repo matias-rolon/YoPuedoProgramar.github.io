1d51a7d92515d489974594464af825b49f92189b
Matias Rolon <matiasrolon1301@gmail.com>
2023-04-03 21:51:05 -0300

    Pagina subida

---
+++ js/botones.js
@@ -0,0 +1,37 @@
+function mostrar1() {
+    document.getElementById("img1").style.width= "20px"
+}
+function mostrar2() {
+    document.getElementById("img2").style.width= "20px"
+}
+function mostrar3() {
+    document.getElementById("img3").style.width= "20px"
+}
+function mostrar4() {
+    document.getElementById("img4").style.width= "20px"
+}
+function mostrar5() {
+    document.getElementById("img5").style.width= "20px"
+}
+function mostrar6() {
+    document.getElementById("img6").style.width= "20px"
+}
+function mostrar7() {
+    document.getElementById("img7").style.width= "20px"
+}
+function mostrar8() {
+    document.getElementById("img8").style.width= "20px"
+}
+function mostrar9() {
+    document.getElementById("img9").style.width= "20px"
+}
+function mostrar10() {
+    document.getElementById("img10").style.width= "20px"
+}
+function mostrar11() {
+    document.getElementById("img11").style.width= "20px"
+}
+function mostrar12() {
+    document.getElementById("img12").style.width= "20px"
+}
+ 
